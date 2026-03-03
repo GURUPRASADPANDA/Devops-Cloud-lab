7e3ddcc6b87b5e246808c6bbe76e0e8f6c008db7
guruprasadpanda <guruprasadpanda4646@gmail.com>
2026-03-03 23:40:34 +0530

    v-1

--- devops-sandbox/terraform/modules/s3/main.tf
+++ devops-sandbox/terraform/modules/s3/main.tf
@@ -15,3 +15,13 @@ resource "aws_s3_bucket_versioning" "main" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
